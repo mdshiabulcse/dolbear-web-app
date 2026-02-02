4b0bea013b6a5fdd0eb5445d68e5f7d9ddffcd3f
Shiabul Islam <md.shiabul.cse@gmail.com>
2026-02-02 20:09:47 +0600

    google tag manager add

--- resources/views/frontend/master.blade.php
+++ resources/views/frontend/master.blade.php
@@ -172,9 +172,40 @@
     @if(settingHelper('is_facebook_pixel_activated') && settingHelper('facebook_pixel_id'))
         {!! base64_decode(settingHelper('facebook_pixel_id')) !!}
     @endif
+
+    <!-- Google Tag Manager -->
+    <script>(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
+    new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
+    j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
+    'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
+    })(window,document,'script','dataLayer','GTM-54BWTWX9');</script>
+    <!-- End Google Tag Manager -->
+
+
 </head>
 
 <body class="sg-active">
+<!-- Google Tag Manager (noscript) -->
+<noscript><iframe src="https://www.googletagmanager.com/ns.html?id=GTM-54BWTWX9"
+                  height="0" width="0" style="display:none;visibility:hidden"></iframe></noscript>
+<!-- End Google Tag Manager (noscript) -->
+
+<!-- Hotjar Tag Manager (noscript) -->
+
+<script src="https://t.contentsquare.net/uxa/f81260905e668.js"></script>
+<!-- End Hotjar Tag Manager (noscript) -->
+
+<!-- clarity Tag Manager (noscript) -->
+<script type="text/javascript">
+    (function(c,l,a,r,i,t,y){
+        c[a]=c[a]||function(){(c[a].q=c[a].q||[]).push(arguments)};
+        t=l.createElement(r);t.async=1;t.src="https://www.clarity.ms/tag/"+i;
+        y=l.getElementsByTagName(r)[0];y.parentNode.insertBefore(t,y);
+    })(window, document, "clarity", "script", "vb27b2096q");
+</script>
+
+<!-- End clarity Tag Manager (noscript) -->
+
 <div id="37142846" class="sg-37142846"></div>
 <div id="app">
     <frontend_master :languages="{{ json_encode($languages) }}" :pages="{{ json_encode($pages) }}" :currencies="{{ json_encode($currencies) }}"
